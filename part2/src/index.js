770f86871b6ead88d3a1cc5ab90d6cee4e0de681
Masaya <sudamasaya0925@gmail.com>
2021-11-30 13:47:38 +0900

    index.jsにnotesデータを持つ最後のコミット これから先はサーバーから取得&ブランチ使用

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,6 +1,14 @@
 import ReactDOM from 'react-dom'
 import App from './App'
+import axios from 'axios'
 
+const promise = axios.get('http://localhost:3001/notes')
+promise.then(response => {
+  console.log(response)
+})
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
 
 const notes = [
   {
