3543b6dac25fe4113e703d8bf8d5ba3f2576be67
Masaya <sudamasaya0925@gmail.com>
2021-12-30 01:46:27 +0900

    part2-7

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,10 +2,22 @@ import React, {useState, useEffect} from 'react'
 import Note  from './components/Note'
 import noteService from './services/notes'
 
+const Notification = ({message}) => {
+  if(message === null) {
+    return null
+  }
+  return (
+    <div className = 'error'>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happend...')
 
   useEffect(() => {
     console.log('effect')
@@ -46,7 +58,8 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(`the note '${note.content}' was already deleted from server`)
+        setErrorMessage(`Note '${note.content}' was already removed from server`)
+        setTimeout(() => {setErrorMessage(null)}, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
       console.log(`importance of ${id} needs to be toggled`)
@@ -62,6 +75,7 @@ const App = () => {
   return (
     <div>
     <h1>Notes</h1>
+    <Notification message = {errorMessage}/>
       <div>
         <button onClick = {() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
