953cc9c4a252fe4b282092a8eda082788488eb76
Masaya <sudamasaya0925@gmail.com>
2022-02-18 16:12:55 +0900

    part3-5

--- part3/index.js
+++ part3/index.js
@@ -1,13 +1,20 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const app = express()
 const Note = require('./models/note')
 
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  if(error.name === 'CastError'){
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
 
 const requestLogger = (request, response, next) => {
   console.log('Method: ',request.method)
@@ -29,10 +36,16 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+    if(note) { 
+       response.json(note)
+    } else {
+       response.status(404).end()
+    }
   })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -56,11 +69,27 @@ app.post('/api/notes', (request, response) => {
 
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, {new : true})
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -68,7 +97,7 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
-
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
