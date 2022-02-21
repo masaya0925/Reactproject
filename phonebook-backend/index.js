f9b20b98301724e2fe4dc3895814bb26cdf12021
Masaya <sudamasaya0925@gmail.com>
2022-02-22 02:09:04 +0900

    end exercise 3.18

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -8,6 +8,15 @@ const Person = require('./models/person')
 app.use(express.static("build"))
 app.use(express.json())
 app.use(cors())
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  if(error.name === 'CastError'){
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+
 //custom formatを使ってHTTP POST request dataを追加 
 app.use(morgan((tokens, request, response) => {
     return [
@@ -20,40 +29,33 @@ app.use(morgan((tokens, request, response) => {
     ].join(' ')
   }))
 
-
 app.get("/api/persons", (request, response) => {
   Person.find({}).then(person => {
     response.json(person)
   })
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if(person){
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 });
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
- // const sameName = persons.find(person => person.name === body.name)
 
     if(!body.name || !body.number) {
         return response.status(400).json({
             error: 'The name or number is missing'
         })
     }
-    /*
-    if(sameName) {
-        return response.status(400).json({
-            error: 'The name already exists in the phonebook'
-        })
-    }
-    */
+    
     const person = new Person ({
       name: body.name,
       number: body.number,
@@ -65,24 +67,42 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.put('/api/persons/:id', (request, response) => {
+  const body = request.body
 
-    response.status(204).end()
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  .then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
+}) 
+
+app.delete('/api/persons/:id', (request, response, next) => {
+   Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.get("/info", (request, response) => {
-  response.send(`
+  Person.countDocuments({})
+  .then(result => {
+    response.send(`
     <div>    
-          <p>Phonebook has info for ${persons.length} people</p>
+          <p>Phonebook has info for ${result} people</p>
           <p>${new Date()}</p>
     </div>
-     `);
+    `)
+  })
 });
 
+app.use(errorHandler)
 const PORT = process.env.PORT
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
