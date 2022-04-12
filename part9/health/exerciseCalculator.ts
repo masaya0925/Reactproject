853fd75e02f6a55a08edf1e3d366b2608a681922
Masaya <sudamasaya0925@gmail.com>
2022-04-12 16:29:14 +0900

    end exercise 9.7

--- part9/health/exerciseCalculator.ts
+++ part9/health/exerciseCalculator.ts
@@ -11,6 +11,7 @@ interface RatingObject {
     rating: number
     ratingDescription: string
 }
+/*
 interface DaysHour {
     hour: Array<number>
     target:number
@@ -18,42 +19,42 @@ interface DaysHour {
 
 const parseExerciseArguments = (args: Array<string>): DaysHour => {
     if(args.length !== 10) {
-         throw new Error('Not enough or too many arguments') //引数が足らないとErrorを投げる
+         throw new Error('Not enough or too many arguments'); //引数が足らないとErrorを投げる
     }
-    const target = Number(args[2])
-    const hour = args.slice(3).map(num => Number(num))  //targetの次の引数から新たな配列を作成
+    const target = Number(args[2]);
+    const hour = args.slice(3).map(num => Number(num));  //targetの次の引数から新たな配列を作成
 
     if(!isNaN(target) && hour.filter(num => isNaN(num)).length === 0){ //無効な値だったらError投げる 
-        return {target, hour}
+        return {target, hour};
     } else {
-        throw new Error('must be number')
+        throw new Error('must be number');
     }
-}
-
+};
+*/
 const averageRate = (daily: number, target: number): RatingObject => {
-    const rate = daily / target
+    const rate = daily / target;
 
     if(rate < 0.5){
-        return {rating: 1, ratingDescription: 'bad'}
+        return {rating: 1, ratingDescription: 'bad'};
     } else if(rate < 1.2) {
-        return {rating: 2, ratingDescription: 'not too bad but could better'}
+        return {rating: 2, ratingDescription: 'not too bad but could better'};
     } else if(rate < 1.5) {
-        return {rating: 3, ratingDescription: 'good'}
+        return {rating: 3, ratingDescription: 'good'};
     } else if(rate < 2) {
-        return {rating: 4, ratingDescription: 'great'}
+        return {rating: 4, ratingDescription: 'great'};
     } else if(rate >= 2.5) {
-        return {rating: 5, ratingDescription: 'perfect'}
+        return {rating: 5, ratingDescription: 'perfect'};
     } else {
-        return {rating: 0, ratingDescription:'invalid number'}
+        return {rating: 0, ratingDescription:'invalid number'};
     }
-}
+};
 
-const calculateExercises = (args: Array<number>, target: number): ResultObject => {
-    const periodLength = args.length
-    const trainingDays = args.filter((days) => days !== 0).length
-    const average = args.reduce((sum, hour) => sum + hour, 0) / args.length
-    const success = average >= target
-    const {rating, ratingDescription} = averageRate(average, target)
+export const calculateExercises = (args: Array<number>, target: number): ResultObject => {
+    const periodLength = args.length;
+    const trainingDays = args.filter((days) => days !== 0).length;
+    const average = args.reduce((sum, hour) => sum + hour, 0) / args.length;
+    const success = average >= target;
+    const {rating, ratingDescription} = averageRate(average, target);
     return {
         periodLength,
         trainingDays,
@@ -62,7 +63,7 @@ const calculateExercises = (args: Array<number>, target: number): ResultObject =
         ratingDescription,
         target,
         average
-    }
-}
-const {target, hour} = parseExerciseArguments(process.argv)
-console.log(calculateExercises(hour, target))
+    };
+};
+//const {target, hour} = parseExerciseArguments(process.argv);
+//console.log(calculateExercises(hour, target));
--- part9/health/index.ts
+++ part9/health/index.ts
@@ -1,25 +1,52 @@
-import express from 'express'
-import { calculateBmi } from './bmiCalculator' // モジュールをimportする
-const app = express()
+import express from 'express';
+import { calculateBmi } from './bmiCalculator'; // モジュールをimportする
+import { calculateExercises } from './exerciseCalculator';
+const app = express();
 
+app.use(express.json());
 app.get('/bmi', (req, res) =>{
   try {
-    const height = Number(req.query.height) //クエリ文字列から身長を取得(Number型)
-    const weight = Number(req.query.weight) //クエリ文字列から体重を取得(Number型)
+    const height = Number(req.query.height); //クエリ文字列から身長を取得(Number型)
+    const weight = Number(req.query.weight); //クエリ文字列から体重を取得(Number型)
 
     if(isNaN(height) || isNaN(weight)) {    //不正な値がないか確認
-      throw new Error('malformatted parameters')  
+      throw new Error('malformatted parameters');  
     } 
 
-    res.send({height, weight, bmi: calculateBmi(height, weight)}) //HTTPレスポンスを送信
+    res.send({height, weight, bmi: calculateBmi(height, weight)}); //HTTPレスポンスを送信
 
   } catch (error){
-      throw new Error(error.message)   
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      res.status(400).send({error: String(error.message)});   
   }
-})
+});
 
-const PORT = 3002
+app.post('/exercise', (req, res) => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const {daily_exercises, target} = req.body;
+
+    if(daily_exercises === undefined || target === undefined) { //undefinedじゃないか検証 
+      throw new Error("parameter missing");
+    }
+    if(!Array.isArray(daily_exercises)) { // daily_exerciseが配列かどうか検証
+      throw new Error("malformatted parameters");
+    }
+    const daily_exercises_num = daily_exercises.map(num => Number(num)); //Number型にした新しい配列を作成
+    const target_num = Number(target);
+
+    if(isNaN(target_num) || daily_exercises_num.filter(num => isNaN(num)).length !== 0) { //targetがNumberじゃないか配列daily_exercise_numの要素にNumber以外が入っていないか検証
+      throw new Error("parameter missing");
+    }
+    res.json(calculateExercises(daily_exercises_num, target_num)); //JSONを送信
+  } catch (error){
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    res.status(400).send({error: String(error.message)});
+  }
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
-    console.log(`Server running on PORT: ${PORT}`)
-})
+    console.log(`Server running on PORT: ${PORT}`);
+});
