9fde4a458a4aa301321b1c571883dfac4da2e2e3
Masaya <sudamasaya0925@gmail.com>
2022-05-16 15:55:14 +0900

    end exercise 9.15

--- part9/courseinfo/src/App.tsx
+++ part9/courseinfo/src/App.tsx
@@ -1,24 +1,91 @@
-
-interface Parts {
-  name: string,
-  exerciseCount: number
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
 }
+
+interface CoursePartV2 extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CoursePartV2 {
+  type: 'normal';
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: 'groupProject';
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartV2 {
+  type: 'submission';
+  exerciseSubmissionLink: string;
+}
+
+interface CourseSpecialPart extends CoursePartV2 {
+  type: 'special';
+  requirements: string[];
+}
+
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`);
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
 
 const Header = ({courseName}: {courseName: string}) => {
   return <h1>{courseName}</h1>;
 };
 
-const Content = ({courseParts}: {courseParts: Parts[]}): JSX.Element => {
+const Content = ({courseParts}: {courseParts: CoursePart[]}): JSX.Element => {
   return (
     <>
-      <p>{courseParts[0].name} {courseParts[0].exerciseCount}</p>
-      <p>{courseParts[1].name} {courseParts[1].exerciseCount}</p>
-      <p>{courseParts[2].name} {courseParts[2].exerciseCount}</p>
+      <p>{courseParts.map((coursePart) => (
+        <Part key={coursePart.name} coursePart = {coursePart}/>
+         ))}
+      </p>
     </>
   );
 };
 
-const Total = ({courseParts}: {courseParts: Parts[]}): JSX.Element => {
+const Part = ({coursePart}: {coursePart: CoursePart}): JSX.Element => {
+    switch(coursePart.type) {
+      case 'normal':
+        return(
+          <>
+          <h4>{coursePart.name} {coursePart.exerciseCount}</h4>
+          <i>{coursePart.description}</i>
+          </>
+        );
+      case 'groupProject':
+        return(
+          <>
+          <h4>{coursePart.name} {coursePart.exerciseCount}</h4>
+          <p>project exercises {coursePart.groupProjectCount}</p>
+          </>
+        );
+      case 'submission':
+        return(
+          <>
+          <h4>{coursePart.name} {coursePart.exerciseCount}</h4>
+          <i>{coursePart.description}</i>
+          <p>submit to {coursePart.exerciseSubmissionLink}</p>
+          </>
+        );
+        case 'special':
+          return(
+            <>
+            <h4>{coursePart.name} {coursePart.exerciseCount}</h4>
+            <i>{coursePart.description}</i>
+            <p>required skills: {coursePart.requirements.join(', ')}</p>
+            </>
+          );
+        default:
+          return assertNever(coursePart);
+    }
+};
+
+const Total = ({courseParts}: {courseParts: CoursePart[]}): JSX.Element => {
   const total = courseParts.reduce((carry, parts) => carry + parts.exerciseCount, 0);
   return <p>
             Number of Exercises{' '}
@@ -28,20 +95,41 @@ const Total = ({courseParts}: {courseParts: Parts[]}): JSX.Element => {
 
 const App = () => {
   const courseName = 'Half stack application development';
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: 'This is the leisured course part',
+      type: 'normal'
+    },
+    {
+      name: 'Advanced',
+      exerciseCount: 7,
+      description: 'This is harded course part',
+      type: 'normal'
     },
     {
       name: 'Using props to pass data',
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: 'groupProject'
     },
     {
       name: 'Deeper type usage',
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: 'Confusing description',
+      exerciseSubmissionLink: 'https://example.fake.com',
+      type: 'submission'
+    },
+    {
+      name: 'Backend development',
+      exerciseCount: 21,
+      description: 'Typing the backend',
+      requirements: ['nodejs', 'jest'],
+      type: 'special'
     }
   ]
+
   return (
     <>
     <Header  courseName  = {courseName}  />
