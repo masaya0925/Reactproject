03892f9b21e8c0ce2469ff2bc55d727f5a21c8af
Masaya <sudamasaya0925@gmail.com>
2022-06-09 16:11:06 +0900

    end exercise 4.4

--- Blogs/src/utils/list_helper.ts
+++ Blogs/src/utils/list_helper.ts
@@ -4,3 +4,6 @@ export const dummy = (_blogs: BlogType[]): number => {
     return 1;
 };
 
+export const totalLikes = (blogs: BlogType[]): number => {
+    return  blogs.reduce((sum, blog) => (sum += blog.likes), 0);
+};
--- Blogs/src/utils/types.ts
+++ Blogs/src/utils/types.ts
@@ -1,6 +1,8 @@
 export type BlogType = {
+  _id: string,
   title: string,
   author: string,
   url: string,
-  likes: number
+  likes: number,
+  __v: number
 };
