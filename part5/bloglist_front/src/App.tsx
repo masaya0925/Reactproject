3e06af8cbd837381de8ac8b7144eee6444a65f7f
Masaya <sudamasaya0925@gmail.com>
2022-11-14 21:35:54 +0900

    end exercise 5.4

--- part5/bloglist_front/src/App.tsx
+++ part5/bloglist_front/src/App.tsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import  axios  from 'axios';
+import { Alert } from '@mui/material';
+
 import { SingleBlog } from './components/Blog';
 import { getAll, setToken, create } from './services/blogs';
 import { login } from './services/login';
@@ -6,8 +9,8 @@ import { Blog, UserToken } from './utils/types';
 
 const App = () => {
   const [blogs, setBlogs] = useState<Blog[]>([]);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [successMessage, setSuccessMessage] = useState<string>('');
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [title, setTitle] = useState<string>('');
@@ -16,10 +19,17 @@ const App = () => {
   const [user, setUser] = useState<UserToken | null>(null);
 
   useEffect(() => {
-   void getAll()
-    .then(blogs => {
-      setBlogs(blogs);
-    });
+    void(async() => {
+      try {
+        const blogs = await getAll();
+        setBlogs(blogs);
+      } catch (e) {
+        setErrorMessage('Unable to retrieve Blogs');
+        setTimeout(() => {
+          setErrorMessage('');
+        }, 5000);
+      }
+    })();
   }, []);
 
   useEffect(() => {
@@ -41,23 +51,33 @@ const App = () => {
         window.localStorage.setItem(
           'loggedBlogappUser', JSON.stringify(user)
         );
-
+        
         setToken(user.token);
         setUser(user);
         setUsername('');
         setPassword('');
-      } catch (exception) {
-        setErrorMessage('Wrong exception');
+        setSuccessMessage(`Welcome ${user.username}.`);
         setTimeout(() => {
-          setErrorMessage(null);
+          setSuccessMessage('');
         }, 5000);
+      } catch (err) {
+        if(axios.isAxiosError(err)){
+           setErrorMessage(JSON.stringify(err.response?.data.error));
+          setTimeout(() => {
+           setErrorMessage('');
+          }, 5000);
+        }
       }
     })(); 
   };
 
   const handleLogout = () => {
-   setUser(null);
-   window.localStorage.removeItem('loggedBlogappUser');
+    setUser(null);
+    window.localStorage.removeItem('loggedBlogappUser');
+    setSuccessMessage('Logged out.');
+    setTimeout(() => {
+     setSuccessMessage('');
+    }, 5000);
   };
 
   const loginForm = () => (
@@ -76,65 +96,95 @@ const App = () => {
     </form>
   );
 
-const addBlog = (event: React.FormEvent<HTMLFormElement>) => {
-  void(async() => {
-    event.preventDefault();
-    const blogObj = {
-      title: title,
-      author: author,
-      url: url,
-      likes: 0
-    };
+  const addBlog = (event: React.FormEvent<HTMLFormElement>) => {
+    void(async() => {
+      event.preventDefault();
+      try {
+        const blogObj = {
+          title: title,
+          author: author,
+          url: url,
+          likes: 0
+        };
+    
+        const returnedBlog = await create(blogObj);
+        setBlogs(blogs.concat(returnedBlog));
+        setTitle('');
+        setAuthor('');
+        setUrl('');
+        setSuccessMessage(`A new blog "${blogObj.title}" by ${blogObj.author} added.`);
+        setTimeout(() => {
+          setSuccessMessage('');
+        }, 5000);
+      } catch (err) {
+        if(axios.isAxiosError(err)){
+          setErrorMessage(JSON.stringify(err.response?.data.error));
+          setTimeout(() => {
+            setErrorMessage('');
+          }, 5000);
+        }
+      }
+    })();
+  };
 
-    const returnedBlog = await create(blogObj);
-    setBlogs(blogs.concat(returnedBlog));
-    setTitle('');
-    setAuthor('');
-    setUrl('');
-  })();
-};
+  const createBlogForm = () => (
+    <form onSubmit = {addBlog}>
+      <div>
+        title
+        <input type = 'text' value = {title} name = 'Title' 
+          onChange = {({ target }) => setTitle(target.value)}/>
+      </div>
+      <div>
+        author
+        <input type = 'text' value = {author} name = 'Author'
+          onChange = {({ target }) => setAuthor(target.value)}/>
+      </div>
+      <div>
+        url
+        <input type = 'text' value = {url} name = 'Url'
+          onChange = {({ target }) => setUrl(target.value)}/>
+      </div>
+      <button type = 'submit'>create</button>
+    </form>
+  );
 
-const createBlogForm = () => (
-  <form onSubmit = {addBlog}>
+  const renderLoginForm = () => (
     <div>
-      title
-      <input type = 'text' value = {title} name = 'Title' 
-        onChange = {({ target }) => setTitle(target.value)}/>
+      <h2>log in to application</h2>
+      {loginForm()}
     </div>
-    <div>
-      author
-      <input type = 'text' value = {author} name = 'Author'
-        onChange = {({ target }) => setAuthor(target.value)}/>
-    </div>
-    <div>
-      url
-      <input type = 'text' value = {url} name = 'Url'
-        onChange = {({ target }) => setUrl(target.value)}/>
-    </div>
-    <button type = 'submit'>create</button>
-  </form>
-);
+  );
 
-  return (
+  const renderBlogList = () => (
     <div>
-      {user === null ?
-        <div>
-          <h2>log in to application</h2>
-          {loginForm()}
-        </div> : 
-        <div>
-          <h2>blogs</h2>
-          <p>{user.name} logged-in 
-          <button type= 'button' onClick={handleLogout}>logout</button>
-          </p>
-          <h2>create new</h2>
-          {createBlogForm()}
+      <h2>blogs</h2>
+       <p>{user?.name} logged-in 
+        <button type= 'button' onClick={handleLogout}>logout</button>
+       </p>
+      <h2>create new</h2>
+        {createBlogForm()}
           {blogs.map(blog => (
             <SingleBlog key = {blog.id} blog = {blog} />
-          ))}
-        </div> 
-      }
+        ))}
     </div>
+  );
+
+  const renderErrorMessage = () => (
+    <Alert severity= 'error'>{errorMessage}</Alert>
+  );
+
+  const renderSuccessMessage = () => (
+    <Alert severity= 'success'>{successMessage}</Alert>
+  );
+
+  return (
+    <>
+    {errorMessage !== '' && renderErrorMessage()}
+    {successMessage !== '' && renderSuccessMessage()}
+    <div>
+      {user === null ? renderLoginForm() : renderBlogList()}
+    </div>
+    </>
   );
 };
 
