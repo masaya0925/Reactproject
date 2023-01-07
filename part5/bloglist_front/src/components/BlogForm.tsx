a2be9a657c13ad74bb56a897ea1dc18748e97d8f
Masaya <sudamasaya0925@gmail.com>
2023-01-08 02:15:02 +0900

    end exercise 5.16

--- part5/bloglist_front/src/components/BlogForm.tsx
+++ part5/bloglist_front/src/components/BlogForm.tsx
@@ -30,20 +30,23 @@ export const BlogForm: React.FC<Props> = ({ createBlog }) => {
       <form onSubmit = {addBlog}>
         <div>
           title
-          <input value = {title} 
+          <input    id = "title"
+                 value = {title} 
               onChange = {({ target }) => setTitle(target.value)}/>
         </div>
         <div>
           author
-          <input value = {author} 
+          <input    id = "author"
+                 value = {author} 
               onChange = {({ target }) => setAuthor(target.value)}/>
         </div>
         <div>
           url
-          <input value = {url} 
+          <input    id = "url"
+                 value = {url} 
               onChange = {({ target }) => setUrl(target.value)}/>
         </div>
-        <button type = 'submit'>create</button>
+        <button id = "submit" type = 'submit'>create</button>
       </form>
     </>
   );
